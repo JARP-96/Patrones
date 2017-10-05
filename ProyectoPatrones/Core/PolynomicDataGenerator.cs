c4a0599bc296509ee88f9d5c29f518e3ba4b4d0c
JARP-96 <jarp9665@gmail.com>
2017-10-05 11:11:09 -0500

    Lo mio por ahora

---
+++ ProyectoPatrones/Core/PolynomicDataGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    internal class PolynomicDataGenerator : IDataGenerator
+    {
+        public List<double> GenerateData()
+        {
+            var rtnList = new List<double>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                //
+            }
+            return rtnList;
+        }
+    }
+}
---
+++ ProyectoPatrones/Core/SquareDataGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    internal class SquareDataGenerator : IDataGenerator
+    {
+        public List<double> GenerateData()
+        {
+            var rtnList = new List<double>();
+            for (int i=0; i < 50; i++)
+            {
+                rtnList.Add(i * 2);
+            }
+            return rtnList;
+        }
+    }
+}
